c2bf2e2f0a1f62b73ea383233bbc8cf2f8dadba2
aschmocker <schmocker@puzzle.ch>
2017-11-10 16:00:56 +0100

    Implemented score counting and added some tests

--- cponktetschecker/ponktechecker.c
+++ cponktetschecker/ponktechecker.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+#include<stdint.h>
 #include<errno.h>
 
 /*   Field numbering
@@ -10,45 +11,33 @@
  *    4   5   6
  *      7   8
  *        9
- *
- *  #define FIG_DRAGN1 0b111010001
- *  #define FIG_DRAGN2 0b100010111
- *  #define FIG_BRACEL 0b110010101
- *  #define FIG_BRACER 0b101010011
- *  #define FIG_CIRCLE 0b111101111
- *  #define FIG_STRIKE 0b111111111
- *  #define FIG_MDROAD 0b100010001
- *  #define FIG_LROADL 0b010000100
- *  #define FIG_LROADR 0b001000010
- *  #define FIG_ARROW1 0b111111001
- *  #define FIG_ARROW2 0b100111111
  */
 
 /* Definitions */
 #define STR_LEN 100 // max string length
 #define N_FIGS 11 // number of possible figures
 
-#define FIG_1 0x1d1 // dragon
-#define FIG_2 0x117 // reverse dragon
-#define FIG_3 0x195 // curly brace left
-#define FIG_4 0x153 // curly brace right
-#define FIG_5 0x1ef // hollow circle
-#define FIG_6 0x1ff // filled circle
-#define FIG_7 0x111 // middle road
-#define FIG_8 0x84 // left little road
-#define FIG_9 0x42 // right little road
-#define FIG_10 0x1f9 // arrow
-#define FIG_11 0x13f // reverse arrow
+#define FIG_1 0b111010001 // 0x1d1 dragon 
+#define FIG_2 0b100010111 // 0x117 reverse dragon
+#define FIG_3 0b110010101 // 0x195 curly brace left
+#define FIG_4 0b101010011 // 0x153 curly brace right
+#define FIG_5 0b111101111 // 0x1ef hollow circle
+#define FIG_6 0b111111111 // 0x1ff filled circle
+#define FIG_7 0b100010001 // 0x111 middle road
+#define FIG_8 0b010000100 // 0x84 little road left
+#define FIG_9 0b001000010 // 0x42 little road right
+#define FIG_10 0b111111001 // 0x1f9 arrow
+#define FIG_11 0b100111111 // 0x13f reverse arrow
 
 /* Figure data type */
 typedef struct figure_s {
-    char fignumber;
-    char message[STR_LEN];
-    unsigned int pattern;
-    unsigned int points;
+    uint8_t fignumber;
+    uint8_t message[STR_LEN];
+    uint16_t pattern;
+    uint16_t points;
 } figure_t;
 
-/* Rule set for the figures */
+/* Score set and "pretty name" for each figure */
 figure_t figures[N_FIGS] = {
     {0, "Drache", FIG_1, 17},
     {1, "Vercherta Drache", FIG_2, 34},
@@ -64,7 +53,7 @@ figure_t figures[N_FIGS] = {
 };
 
 /* Abort on failure */
-void die(const char *message)
+static void die(const char *message)
 {
     if(errno) {
         perror(message);
@@ -76,9 +65,9 @@ void die(const char *message)
 }
 
 /* Turn a binary string into an integer */
-unsigned int binstr2int(char *str)
+static uint16_t binstr2int(char *str)
 {
-    int i = 0;
+    uint16_t i = 0;
 
     while (*str != '\0' && *str != '\n') {
         if (*str != '0' && *str != '1') {
@@ -90,11 +79,23 @@ unsigned int binstr2int(char *str)
     return i;
 }
 
-/* Check incoming pattern for matches return figumber or -1 if no match*/
-int check_figures(unsigned int in)
+/* Count the number of set bits in an integer */
+static uint16_t count_set_bits(uint16_t in)
 {
-    for(int i=0; i<(N_FIGS-1); i++) {
-        if(in==figures[i].pattern) {
+    uint16_t c;
+
+    for (c = 0; in; c++) {
+        in &= in-1; // clear lsb
+    }
+
+    return c;
+}
+
+/* Check incoming pattern for matches return figumber or -1 if no match*/
+static int check_figures(uint16_t in)
+{
+    for(int i=1; i<(N_FIGS); i++) {
+        if(in == figures[i].pattern) {
             return figures[i].fignumber;
         }
     }
@@ -104,7 +105,7 @@ int check_figures(unsigned int in)
 
 int main(int argc, char **argv)
 {
-    unsigned int input=0;
+    uint16_t input=0;
     int n=0;
 
     if(argc!=2) {
@@ -113,15 +114,12 @@ int main(int argc, char **argv)
 
     input = binstr2int(argv[1]);
 
-    printf("[DEBUG]: Input: %x\n", input); // Debug print
+    printf("[DEBUG]: Input: %d\n", input); // Debug print
 
     if((n=check_figures(input))>0) {
         printf("Figur: %s\nPunkte: %d\n", figures[n].message, figures[n].points);
     } else {
-        printf("%d",n);
-        printf("No figure, calculating points ...\n");
-        // TODO Add all binary 1's and return points
-        // ...
+        printf("Keine Figur.\nPunkte: %d\n", count_set_bits(input));
     }
 
     return EXIT_SUCCESS;
